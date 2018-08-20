9a6afa3a6d40f66ec283af42f08e32b25989df86
Cheik Baboulak <cheikbaboulak@gmail.com>
2018-08-19 18:36:08 -0700

    foundersrooms spring backend 201808200235

---
+++ src/main/java/com/foundersrooms/elasticsearchreprository/UserElasticRepository.java
@@ -0,0 +1,15 @@
+package com.foundersrooms.elasticsearchreprository;
+
+import java.util.Set;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.foundersrooms.domain.people.User;
+
+public interface UserElasticRepository extends ElasticsearchRepository<User, Long> {
+	User findByUsername(String username);
+	/*
+	Set<User> findByElasticContacts_lastNameContainingAllIgnoreCaseOrderByLastNameAsc(String namePart);
+	Set<User> findByElasticContacts_completeNameContainingAllIgnoreCaseOrderByLastNameAsc(String namePart);
+	*/
+}
